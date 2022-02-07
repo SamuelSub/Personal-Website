47bd7f34d9527a3935494eeec6be3f97e733ea15
samuel <samsubas01@gmail.com>
2022-02-07 15:01:45 +0200

    portfolio site

--- src/script.js
+++ src/script.js
@@ -2,34 +2,69 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
+gsap.registerPlugin(ScrollTrigger, scrollTo);
 // Debug
 const gui = new dat.GUI()
-
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
-
 // Scene
 const scene = new THREE.Scene()
 
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+let tl = gsap.timeline();
 
-// Materials
+tl.from('.hidetext', {
+    delay: .5,
+    duration: 1,
+    ease: 'power4.out',
+    stagger: 0.2,
+    y: 100
+}, 0)
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+tl.to('.hidetext', {
+    color: '#E63946',
+    delay: 0.7
+}, 0)
 
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+
+// Loader
+const loader = new GLTFLoader();
+
+let laptop;
+
+loader.load('phone.glb', (glb) => {
+    laptop = glb;
+    scene.add(glb.scene)
+    gsap.to(glb.scene.position, {
+        duration: 1.5,
+        z: 3.5,
+        x: 4
+        // x: 0.2,
+        // y: -0.4
+    });
+    gsap.to(glb.scene.rotation, {
+        duration: 1.5,
+        y: 2.7,
+    })
+    if(window.innerWidth <= 600) {
+        glb.scene.position.x = 0
+        glb.scene.position.z = -5
+        glb.scene.position.y = 0
+    } else {
+        glb.scene.position.x = 5
+    }
+}, undefined, (err) => {
+    console.error(err);
+})
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+const pointLight = new THREE.PointLight(0xffffff, 1)
+pointLight.position.set(2, 0, 10);
+// gui.add(pointLight.position, 'x').min(0).max(100)
+// gui.add(pointLight.position, 'y').min(0).max(100)
+// gui.add(pointLight.position, 'z').min(0).max(100)
 scene.add(pointLight)
 
 /**
@@ -45,11 +80,11 @@ window.addEventListener('resize', () =>
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
-
+    
     // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
-
+    
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -59,29 +94,131 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(40, sizes.width / sizes.height, 0.1, 100)
+// camera.position.x = 10
+// camera.position.y = 2
+camera.position.z = 12
+// camera.position.set(0, 3, 10);
+// camera.up.set(0, 5, 5)
+// camera.lookAt(0, 0, 0);
+
 scene.add(camera)
 
 // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+// const controls = new OrbitControls(camera, canvas);
+
+const laptopAnimation = () => {
+    laptop.scene.position.y = scrollY * 0.003
+    gsap.to('.hidetext', {
+        y: `${scrollY * 0.003}em`
+    });
+    gsap.to('.portfolio', {
+        y: `${scrollY * -0.003}em`
+    })
+}
+
+// gsap.from('.card-1 div', {
+//     scrollTrigger: {
+//         trigger: '.card-1'
+//     },
+//     x: '-100vw',
+//     duration: 1
+// });
+
+
+// CARD 1
+gsap.to('.card-1', {
+    scrollTrigger: {
+        trigger: '.card-1',
+        scrub: 1
+    },
+    scale: 1.2
+})
+
+gsap.to('.hide-card-1-col', {
+    scrollTrigger: {
+        trigger: '.hide-card-1-col',
+        scrub: 7
+    },
+    x: '-100vw'
+})
+
+gsap.to('.hide-card-1-col-2', {
+    scrollTrigger: {
+        trigger: '.hide-card-1-col-2',
+        scrub: 7
+    },
+    x: '100vw'
+})
+
+// CARD 2
+gsap.to('.card-2', {
+    scrollTrigger: {
+        trigger: '.card-2',
+        scrub: 1
+    },
+    scale: 1.2
+})
+
+gsap.to('.hide-card-2-col', {
+    scrollTrigger: {
+        trigger: '.hide-card-2-col',
+        scrub: 5
+    },
+    x: '-100vw'
+})
+
+gsap.to('.hide-card-2-col-2', {
+    scrollTrigger: {
+        trigger: '.hide-card-2-col-2',
+        scrub: 5
+    },
+    x: '100vw'
+})
+
+// CARD 3
+gsap.to('.card-3', {
+    scrollTrigger: {
+        trigger: '.card-3',
+        scrub: 1
+    },
+    scale: 1.2
+})
+
+gsap.to('.hide-card-3-col', {
+    scrollTrigger: {
+        trigger: '.hide-card-3-col',
+        scrub: 5
+    },
+    x: '-100vw'
+})
+
+gsap.to('.hide-card-3-col-2', {
+    scrollTrigger: {
+        trigger: '.hide-card-3-col-2',
+        scrub: 5
+    },
+    x: '100vw'
+})
+
+
+document.addEventListener('scroll', laptopAnimation);
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true,
+    antialias: true
 })
+
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
  */
-
 const clock = new THREE.Clock()
 
 const tick = () =>
@@ -90,9 +227,9 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    // sphere.rotation.y = .5 * elapsedTime
 
-    // Update Orbital Controls
+    // Update Orbital 
     // controls.update()
 
     // Render
